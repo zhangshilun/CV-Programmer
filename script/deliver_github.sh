fc8f20471e071337db5ee035d57f2fa74e641487
zhangshilun <zhangslsl@foxmail.com>
2020-08-20 10:33:49 +0800

    updates Thu Aug 20 10:33:49 CST 2020

---
+++ script/deliver_github.sh
@@ -0,0 +1,9 @@
+﻿cd /home/blog/
+hugo --config config_github.toml
+rm -rf CV-Programmer/docs/
+cp -r public/ CV-Programmer/docs/
+cd CV-Programmer/
+git add docs/
+git commit -m "updates $(date)"
+git push origin master
+echo 'success'
